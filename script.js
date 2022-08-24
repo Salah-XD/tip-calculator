9bbb286f8730a43e3b4cbe8ebf4749c0bca8e656
Mohd. Salah <hudeensala948@gmail.com>
2022-08-24 14:38:27 +0530

    formatter i

---
+++ script.js
@@ -0,0 +1,12 @@
+function man() {
+  console.log("salahudeeen");
+}
+console.log("sala");
+
+const a = "sala";
+print(a);
+document.innerHTML = a;
+document.getElementById = ".sala";
+
+alert("what was that");
+array.forEach((element) => {});
---
+++ test.py
@@ -0,0 +1,8 @@
+list=[1,2,3,4,5]
+print(list)
+for x in list:
+    print(x)
+if list==[1,2,3,4,5]:
+    print("fuck!!")
+else:
+    print("suxk")
